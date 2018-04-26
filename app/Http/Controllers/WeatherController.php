a15f39ae11bfbec6295f7476fec8be798109c9f7
Turner Walters <turner@bythepixel.com>
2018-04-26 14:59:37 -0600

    Some controllers and a route

---
+++ app/Http/Controllers/WeatherController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class WeatherController extends Controller
+{
+
+    /**
+     * Index
+     */
+    public function index()
+    {
+        echo "test";
+    }
+}
